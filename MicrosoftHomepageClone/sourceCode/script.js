9706ecc64117787b14a003982641df390e80081b
Kevin Xiao <aquarius.c.c.a@gmail.com>
2025-07-01 09:32:18 +0800

    docs(MicrosoftHomepageClone): 新增「遮罩層＋側邊菜單」筆記

--- MicrosoftHomepageClone/sourceCode/script.js
+++ MicrosoftHomepageClone/sourceCode/script.js
@@ -1,5 +1,24 @@
-document
-  .querySelector(".menu-btn")
-  .addEventListener("click", () =>
-    document.querySelector(".main-menu").classList.toggle("show")
-  );
+const menuButton = document.querySelector(".menu-btn");
+menuButton.addEventListener("click", () => {
+  const menu = document.querySelector(".main-nav ul.main-menu");
+  const overlay = document.getElementById("overlay");
+
+  // 切換菜單的顯示狀態
+  menu.classList.toggle("show");
+  
+  // 切換遮罩的顯示狀態
+  overlay.classList.toggle("open");
+});
+
+const overlay = document.getElementById("overlay");
+
+// 2. 點按遮罩：只要遮罩可見就關閉菜單
+overlay.addEventListener("click", () => {
+  const menu = document.querySelector(".main-nav ul.main-menu");
+  
+  // 如果菜單是顯示狀態，則關閉菜單
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show");
+    overlay.classList.remove("open");
+  }
+});
